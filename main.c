31be45d0fa12ee96a7b8861c17f825061db289fd
Mateus Soares <mateus123soares@hotmail.com>
2022-06-21 21:24:18 -0300

    refatorando codigo e ajustando variaveis

--- main.c
+++ main.c
@@ -6,33 +6,47 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#include "http_server.h"
+
 int main(){
-    char buff[129];
 
     struct sockaddr_in caddr;
     struct sockaddr_in saddr = {
         .sin_family      = AF_INET,
         .sin_addr.s_addr = htonl(INADDR_ANY),
-        .sin_port        = htons(5000)
+        .sin_port        = htons(5001)
     };
 
-    int server = socket(AF_INET, SOCK_STREAM, 0);
-    int client, x;
+    char buff[MESSAGE_SIZE];
+    int client_connect, message_rsv,server;
     int csize  = sizeof caddr;
 
-    bind(server, (struct sockaddr *) &saddr, sizeof saddr);
-    listen(server, 5);
+    server=socket(AF_INET, SOCK_STREAM, 0);
+    if (server == -1){
+        perror("socket failed");
+        exit(1);
+    }
+
+    if (bind(server, (struct sockaddr *) &saddr, sizeof saddr) == -1){
+        perror("bind failed");
+        exit(1);
+    }
+
+    if(listen(server, MAX_CONNECTIONS) == -1){
+        perror("listen failed");
+        exit(1);
+    }
 
     while(1){
-        client = accept(server, (struct sockaddr *) &caddr, &csize);
-        x = recv(client, buff, sizeof buff, 0);
-
-        send(client, buff, x, 0);
+        client_connect = accept(server, (struct sockaddr *) &caddr, &csize);
+        message_rsv = recv(client_connect, buff, sizeof buff, 0);
+        
+        send(client_connect, buff, client_connect, 0);
 
         puts(buff);
         fflush(stdout);
 
-        close(client);
+        close(client_connect);
     }
 
     return 0;
