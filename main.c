7b089a25aa85a459fd25dac9647bb3926c84175d
ronaldoamato <79540157+ronaldoamato@users.noreply.github.com>
2022-06-27 22:46:08 -0300

    Update main.c
    Tratando requisições com threads

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-
+#include <pthread.h>
 #include "http_server.h"
 
 int main(){
@@ -24,10 +27,17 @@ int main(){
     server = initServer(caddr,saddr);
 
     while(1){
-        client_connect = accept(server, (struct sockaddr *) &caddr, &csize);
-        
-        message_rsv = recv(client_connect, buff, sizeof buff, 0);
-        readFile(client_connect);
+        printf ("Aguardando conexões...\n");
+        if((client_connect = accept(server, (struct sockaddr *) &caddr, &csize))==true){
+            printf ("Conectado!");
+            message_rsv = recv(client_connect, buff, sizeof buff, 0);
+        //readFile(client_connect);
+
+            pthread_t thread;                                       //instancia uma thread caso receba um pedido de conexão
+            int *pcli_socket = malloc(sizeof(int));                 // aloca em heap 
+            *pcli_socket = client_connect;                          // aloca o socket
+            pthread_create(&thread, NULL, readFile, pcli_socket);   // chama a função de tratamento da requisição readFile
+        }
         //send(client_connect, buff, client_connect, 0);
         puts(buff);
         fflush(stdout);
